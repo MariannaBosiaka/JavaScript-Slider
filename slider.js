57edc98b589d38c94a4935df7f1d1c6f7273bf40
Marianna Bosiaka <57630657+MariannaBosiaka@users.noreply.github.com>
2024-06-23 11:25:00 +0300

    added buttons

--- slider.js
+++ slider.js
@@ -2,27 +2,62 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
 const images = document.querySelectorAll(".image");
+const bottom = document.querySelector(".bottom");
+
+
 
 let sliderValue = 1;
 const length = images.length;
 
-left.addEventListener("click", ()=>{
+const changeImages = () => {
 
-    if(sliderValue < length)
-    {
-        console.log("moved the image");
-        // slider.style.transform = 'translateX(-${sliderValue * 500}px)';
+    console.log("moved the image");
+    // slider.style.transform = 'translateX(-${sliderValue * 500}px)';
     
         
-        slider.style.transform = `translateX(-${sliderValue*400}px)`;
-        sliderValue++;
-        
-    }
-    else
-    {
-        slider.style.transform = `translateX(0px)`;
-        sliderValue = 1;
-    }
+    slider.style.transform = `translateX(-${sliderValue*400}px)`;
+    sliderValue++;
 
+}
+
+const goToStart = () => {
+
+    slider.style.transform = `translateX(0px)`;
+    sliderValue = 1;
+
+}
+
+const prevSlide = () => {
+    
+    slider.style.transform = `translateX(-${(sliderValue-2)*400}px)`;
+    sliderValue--;
+}
+
+const lastSlide = () =>{
+
+    slider.style.transform = `translateX(-${(length - 1)*400}px)`;
+    sliderValue = length;
+}
+
+
+right.addEventListener("click", ()=>{
+
+    sliderValue < length ? changeImages() : goToStart();
 
 } )
+
+
+left.addEventListener("click", () =>{
+
+    sliderValue > 1 ? prevSlide() : lastSlide();
+
+})
+
+for(let i = 0; i<length; i++)
+{
+    const div = document.createElement("div");
+    div.className = "button";
+
+    bottom.appendChild(div);
+
+}
