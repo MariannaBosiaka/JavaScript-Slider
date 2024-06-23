a507069ba9dfd293fef84d2b02fe9272c492a4d1
Marianna Bosiaka <57630657+MariannaBosiaka@users.noreply.github.com>
2024-06-23 10:36:40 +0300

    clickable left button and alternate between images

--- slider.js
+++ slider.js
@@ -1,1 +1,28 @@
-console.log("It works yayyy!");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
+
+let sliderValue = 1;
+const length = images.length;
+
+left.addEventListener("click", ()=>{
+
+    if(sliderValue < length)
+    {
+        console.log("moved the image");
+        // slider.style.transform = 'translateX(-${sliderValue * 500}px)';
+    
+        
+        slider.style.transform = `translateX(-${sliderValue*400}px)`;
+        sliderValue++;
+        
+    }
+    else
+    {
+        slider.style.transform = `translateX(0px)`;
+        sliderValue = 1;
+    }
+
+
+} )
